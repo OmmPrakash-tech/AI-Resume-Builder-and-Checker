6575efaf241af8ba2834e0e049741d95ef73ddbe
Omm Prakash Debata <ommprakashdebata6@gmail.com>
2026-05-11 19:53:27 +0530

    Intialize commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <Button>
+        ResumeAI 🚀
+      </Button>
+    </div>
+  )
+}
+
+export default App
